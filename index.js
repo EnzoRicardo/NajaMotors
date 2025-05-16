2d612c9bd52aa98beec1df5f1ca860d284908996
Luan <luanlucho@hotmail.com>
2025-05-16 12:05:17 -0300

    Começando proteção de rotas

--- index.js
+++ index.js
@@ -30,6 +30,60 @@ const db = mysql.createConnection({
     database: 'najamotors'
 });
 
+// API Cadastro de usuário
+app.post('/api/cadastrarU', async (req, res) => {
+    const { nome, email, cpf, senha } = req.body;
+
+    if (!nome || !email || !cpf  || !senha) {
+        return res.status(400).json({ message: 'Os campos são obrigatórios.' });
+    }
+
+    const hashpass = await bcrypt.hash(senha, 10);
+    const q = `INSERT INTO usuario (nome, email, cpf, senha) VALUES (?, ?, ?, ?)`
+    
+    db.query(q, [nome,email,cpf,hashpass], (err,data) => {
+        if (err){
+            if (err.code === 'ER_DUP_ENTRY') {
+                return res.status(409).json({ message: 'Email ou CPF ja cadastrados.'});
+            }
+            return res.json(err)
+        } 
+            
+        return res.json("Usuário registrado.")
+    });
+});
+
+
+// API Update de usuário
+app.put('/api/usuarios/:id', (req, res) => {
+    const { id } = req.params;
+    const { nome, email, cpf } = req.body;
+
+    db.query('UPDATE usuario SET nome = ?, email = ?, cpf = ? WHERE id = ?', [nome, email, cpf, id], (err, results) => {
+        if (err) return res.status(500).json({ message: 'Erro ao atualizar usuário.' });
+        res.status(200).json({ message: 'Usuário atualizado com sucesso!' });
+    });
+});
+
+// API listar usuarios
+app.get('/api/usuarios', (req, res) => {
+    db.query('SELECT * FROM usuario', (err, results) => {
+        if (err) return res.status(500).json({ message: 'Erro ao buscar usuários.' });
+        res.status(200).json(results);
+    });
+});
+
+// API Deletar usuários
+app.delete('/api/usuarios/:id', (req, res) => {
+    const { id } = req.params;
+    db.query('DELETE FROM usuario WHERE id = ?', [id], (err, results) => {
+        if (err) return res.status(500).json({ message: 'Erro ao excluir usuário.' });
+        res.status(200).json({ message: 'Usuário excluído com sucesso!' });
+    });
+});
+
+
+// API Login
 app.post('/api/login', (req, res) => {
     const { email, senha } = req.body;
 
@@ -54,64 +108,14 @@ app.post('/api/login', (req, res) => {
         req.session.user = {
             id: usuario.id,
             name: usuario.nome,
-            email: usuario.email
+            funcao: usuario.funcao
         }
 
         return res.status(200).json({ message: "Logado com sucesso"});
     })
 });
 
-// API listar
-app.get('/api/usuarios', (req, res) => {
-    db.query('SELECT * FROM usuario', (err, results) => {
-        if (err) return res.status(500).json({ message: 'Erro ao buscar usuários.' });
-        res.status(200).json(results);
-    });
-});
-
-// API Delete
-app.delete('/api/usuarios/:id', (req, res) => {
-    const { id } = req.params;
-    db.query('DELETE FROM usuario WHERE id = ?', [id], (err, results) => {
-        if (err) return res.status(500).json({ message: 'Erro ao excluir usuário.' });
-        res.status(200).json({ message: 'Usuário excluído com sucesso!' });
-    });
-});
-
-// API Update
-app.put('/api/usuarios/:id', (req, res) => {
-    const { id } = req.params;
-    const { nome, email, cpf } = req.body;
-
-    db.query('UPDATE usuario SET nome = ?, email = ?, cpf = ? WHERE id = ?', [nome, email, cpf, id], (err, results) => {
-        if (err) return res.status(500).json({ message: 'Erro ao atualizar usuário.' });
-        res.status(200).json({ message: 'Usuário atualizado com sucesso!' });
-    });
-});
-
-
-// API Create Usuário
-app.post('/api/cadastrarU', async (req, res) => {
-    const { nome, email, cpf, senha } = req.body;
-
-    if (!nome || !email || !cpf  || !senha) {
-        return res.status(400).json({ message: 'Os campos são obrigatórios.' });
-    }
-
-    const hashpass = await bcrypt.hash(senha, 10);
-    const q = `INSERT INTO usuario (nome, email, cpf, senha) VALUES (?, ?, ?, ?)`
-    
-    db.query(q, [nome,email,cpf,hashpass], (err,data) => {
-        if (err){
-            if (err.code === 'ER_DUP_ENTRY') {
-                return res.status(409).json({ message: 'Email ou CPF ja cadastrados.'});
-            }
-            return res.json(err)
-        } 
-            
-        return res.json("Usuário registrado.")
-    });
-});
+// API Logout
 
 app.post('/api/logout', (req, res) => {
     req.session.destroy(err => {
@@ -122,40 +126,14 @@ app.post('/api/logout', (req, res) => {
     })
 })
 
-function authMiddleware(req, res, next) {
-    if (req.session.user) {
-        next();
-    } else {
-        res.status(401).json({ message: 'Não autorizado' });
-    }
-}
-
+// API Autorização de usuário autenticado
 
 app.get('/api/usuario', authMiddleware, (req, res) => {
     res.json({usuario: req.session.user})
 })
 
 
-// API listar Carro
-app.get('/api/carros', (req, res) => {
-    const sql = `
-        SELECT 
-            Carro.id AS id,
-            Carro.ano,
-            CONCAT('R$ ', FORMAT(Carro.preco, 2, 'pt_BR')) AS preco,
-            Modelo.nome AS modelo,
-            Marca.nome AS marca
-        FROM Carro
-        JOIN Modelo ON Carro.modelo_id = Modelo.id
-        JOIN Marca ON Modelo.marca_id = Marca.id
-    `;
-
-    db.query(sql, (err, results) => {
-        if (err) return res.status(500).json({ message: 'Erro ao buscar carros.' });
-        res.status(200).json(results);
-    });
-});
-
+// API Cadastrar Carro
 
 app.post('/api/cadastrarC', (req, res) => {
     const { ano, preco, modelo } = req.body;
@@ -178,6 +156,7 @@ app.post('/api/cadastrarC', (req, res) => {
     });
 });
 
+// API Atualizar Carro
 
 app.put('/api/carro/:id', (req, res) => {
     const { ano, preco, modelo } = req.body;
@@ -201,7 +180,7 @@ app.put('/api/carro/:id', (req, res) => {
 });
 
 
-// API Delete Carro
+// API Deletar Carro
 app.delete('/api/carro/:id', (req, res) => {
     const { id } = req.params;
     db.query('DELETE FROM Carro WHERE id = ?', [id], (err, results) => {
@@ -211,7 +190,43 @@ app.delete('/api/carro/:id', (req, res) => {
 });
 
 
+// API listar Carro
+app.get('/api/carros', (req, res) => {
+    const sql = `
+        SELECT 
+            Carro.id AS id,
+            Carro.ano,
+            CONCAT('R$ ', FORMAT(Carro.preco, 2, 'pt_BR')) AS preco,
+            Modelo.nome AS modelo,
+            Marca.nome AS marca
+        FROM Carro
+        JOIN Modelo ON Carro.modelo_id = Modelo.id
+        JOIN Marca ON Modelo.marca_id = Marca.id
+    `;
 
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ message: 'Erro ao buscar carros.' });
+        res.status(200).json(results);
+    });
+});
+
+// Função Autorização
+
+function authMiddleware(req, res, next) {
+    if (req.session.user) {
+        next();
+    } else {
+        res.status(401).json({ message: 'Não autorizado' });
+    }
+}
+
+const path = require('path');
+
+app.get('/crud', authMiddleware, (req, res) => {
+    res.sendFile(path.join(__dirname, 'home', 'crud.html'));
+});
+
+// Iniciar Servidor
 
 const port = 3000;
 app.listen(port, () => {
